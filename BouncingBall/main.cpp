34806c2f2375acacf3e974b2e004bf73749ec9d1
StripedBanana <jnsgs2@gmail.com>
2016-04-06 23:02:55 +0200

    Changed object collision method.

--- BouncingBall/main.cpp
+++ BouncingBall/main.cpp
@@ -11,6 +11,7 @@
  // Prototypes
 int circlesColliding(Ball* b1, Ball* b2); // returns true if both balls are in contact
 void handleBallCollision(Ball** b1, Ball** b2);   // handles the collision between both balls
+void debug(sf::RenderWindow* window, vector<Ball*> balls); // displaying center & old center of the balls
 
 int main()
 {
@@ -65,6 +66,11 @@ int main()
         balls[i] = new Ball(x + dx, y + dy, x, y, 0, accY, ballRadius, 1);
     }
 
+    balls[0]->setXY(LENGTH/2, HEIGHT/2);
+    balls[0]->setOldXY(LENGTH/2 - 1, HEIGHT/2 - 1);
+    balls[1]->setXY(LENGTH/2 + 100, HEIGHT/2 + 100);
+    balls[1]->setOldXY(LENGTH/2 + 100 - 0.5, HEIGHT/2 + 100 - 0.5);
+
 
     // Process loop
     while (window.isOpen())
@@ -114,12 +120,9 @@ int main()
                 else collision = false;
                 if(collision)
                 {
-                    cout << "avant\t" << "x=" << ballTest->getX() << " y=" << ballTest->getY() << " oldx=" << ballTest->getOldX() << " oldy=" << ballTest->getOldY() << endl;
+                    cout << "collision" << endl;
                     handleBallCollision(&ballTest, &returnBalls[x]);
-                    handleBallCollision(&returnBalls[x], &ballTest);
-                    cout << "apres\t" << "x=" << ballTest->getX() << " y=" << ballTest->getY() << " oldx=" << ballTest->getOldX() << " oldy=" << ballTest->getOldY() << endl;
                 }
-
             }
         }
 
@@ -138,6 +141,7 @@ int main()
         window.draw(fps);
 
         // Updating window
+        debug(&window, balls);
         window.display();
     }
 
@@ -165,15 +169,28 @@ int circlesColliding(Ball* b1, Ball* b2)
 
 void handleBallCollision(Ball** b1, Ball** b2)
 {
-    // Calcul de la base orthonormée (n,g)
 
+    /*
+    // Calcul de la base orthonormée (n,g)
     double dx = (*b1)->getX() - (*b2)->getX();
     double dy = (*b1)->getY() - (*b2)->getY();
     double r = sqrt(dx*dx + dy*dy);
 
+    if(abs((long long)dx)<0.1)
+    {
+        cout << "dx=" << dx << endl;
+        cin.ignore();
+        cin.ignore();
+    }
+    if(abs((long long)dy)<0.1)
+    {
+        cout << "dy=" << dy << endl;
+        cin.ignore();
+        cin.ignore();
+    }
     // n est perpendiculaire au plan de collision
-    double nx = - dx / r;
-    double ny = - dy / r;
+    double nx = -dx;
+    double ny = -dy;
     double mn = ny / nx;
     double pn = (*b1)->getY() - mn * (*b1)->getX();
 
@@ -195,12 +212,55 @@ void handleBallCollision(Ball** b1, Ball** b2)
     double newX = 2*projectionX - (*b1)->getOldX();
     double newY = 2*projectionY - (*b1)->getOldY();
 
-    //cout << "dx=" << dx << " dy=" << dy << " mg=" << mg << " pg=" << pg << " mp=" << mp << " pp=" << pp << endl;
-    //cout << "projectionX=" << projectionX << " projectionY=" << projectionY << endl;
+    // setting up the modifications
     (*b1)->setOldXY((*b1)->getX(), (*b1)->getY());
     (*b1)->setXY(newX, newY);
-    cout << "apres\t" << "x=" << (*b1)->getX() << " y=" << (*b1)->getY() << " oldx=" << (*b1)->getOldX() << " oldy=" << (*b1)->getOldY() << endl;
+
+    // second object
+    pg = (*b2)->getY() - mg * (*b2)->getX();
+    pp = (*b2)->getOldY() - mp * (*b2)->getOldX();
+    projectionX = (pn - pp)/(mp - mn);
+    projectionY = mp * projectionX + pp;
+    newX = 2*projectionX - (*b2)->getOldX();
+    newY = 2*projectionY - (*b2)->getOldY();
+    (*b2)->setOldXY((*b2)->getX(), (*b2)->getY());
+    (*b2)->setXY(newX, newY);
+    */
+
+    float m1 = (*b1)->getM(), m2 = (*b2)->getM();
+    float v1x_i = (*b1)->getX() - (*b1)->getOldX(), v1y_i = (*b1)->getY() - (*b1)->getOldY();
+    float v2x_i = (*b2)->getX() - (*b2)->getOldX(), v2y_i = (*b2)->getY() - (*b2)->getOldY();
+    float v1x_f = (m1-m2)/(m1+m2)*v1x_i + 2*m2/(m1+m2)*v2x_i, v1y_f = (m1-m2)/(m1+m2)*v1y_i + 2*m2/(m1+m2)*v2y_i;
+    float v2x_f = 2*m1/(m1+m2)*v1x_i + (m2-m1)/(m1+m2)*v2x_i, v2y_f = 2*m1/(m1+m2)*v1y_i + (m2-m1)/(m1+m2)*v2y_i;
+
+    cout << "v1x_i=" << v1x_i << " v2x_i=" << v2x_i << endl;
+    cout << "v1x_f=" << v1x_f << " v2x_f=" << v2x_f << endl;
+
+    float b1x = (*b1)->getX(), b1y = (*b1)->getY();
+    float b2x = (*b2)->getX(), b2y = (*b2)->getY();
+
+    (*b1)->setOldXY(b1x, b1y);
+    (*b2)->setOldXY(b2x, b2y);
+
+    (*b1)->setXY(b1x + v1x_f, b1y + v1y_f);
+    (*b2)->setXY(b2x + v2x_f, b2y + v2y_f);
+
+}
+
+void debug(sf::RenderWindow* window, vector<Ball*> balls)
+{
+    sf::CircleShape center(balls[0]->getRadius()/10), oldCenter(balls[0]->getRadius()/10
 
 
+                                                                );
+    center.setFillColor(sf::Color::Green);
+    oldCenter.setFillColor(sf::Color::Red);
+    for(int i=0; i<balls.size(); i++)
+    {
+        center.setPosition(balls[i]->getX(), balls[i]->getY());
+        oldCenter.setPosition(balls[i]->getOldX(), balls[i]->getOldY());
+        window->draw(center);
+        window->draw(oldCenter);
+    }
 }
 
